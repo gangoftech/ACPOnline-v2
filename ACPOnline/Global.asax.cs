ddac81f23e984fd6463e6ded8c4098204b5d0234
181362 <181362@VM2COG-L238>
2016-08-30 03:25:48 -0700

    test

--- ACPOnline/Global.asax.cs
+++ ACPOnline/Global.asax.cs
@@ -16,6 +16,7 @@ namespace ACPOnline
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //dsdsd
         }
     }
 }
